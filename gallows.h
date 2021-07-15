4f599dc4efa80d089ef641aca3783b0b0f804ff1
kevin <me@example.com>
2021-07-15 22:52:57 +0200

    Added makefile

---
+++ gallows.h
@@ -0,0 +1,30 @@
+/* gallows
+    ________
+    |      |
+    |      O
+    |     /|\
+    |     / \
+    |
+    A
+   AAA
+*/
+
+unsigned int tries = 7;
+
+const char* horizontal_bar = "________";
+
+const char* rope = "|";
+
+const char* head = "O";
+
+const  char* upper_body = "/|\\";
+
+const  char* legs = "/ \\";
+
+const char* vertical_bar = "|\n\
+ |\n\
+ |\n\
+ |\n\
+ |\n";
+
+const char* hill = " A \nAAA";
